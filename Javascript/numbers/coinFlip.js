075477f51a88689503c2650e458b8aa4e5a39ba1
JaredCubilla <deraj.allibuc@gmail.com>
2014-08-26 13:00:24 -0400

    Changed typo

--- Javascript/numbers/coinFlip.js
+++ Javascript/numbers/coinFlip.js
@@ -3,11 +3,11 @@ Coin Flip Simulation - Javascript
 */
 
 function coinFlip(count) {
-	var obj = {record: [], headCount: 0, tailsCount: 0};
+	var obj = {record: [], headsCount: 0, tailsCount: 0};
 	for (var i = 0; i < count; i++) {
 		if ((Math.floor((Math.random() * 2) + 1)) === 1) {
 			obj.record.push('Heads');
-			obj.headCount++;
+			obj.headsCount++;
 		} else {
 			obj.record.push('Tails');
 			obj.tailsCount++;
